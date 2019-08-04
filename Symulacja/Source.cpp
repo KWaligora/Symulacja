50ad433b80e9729bb17e037f92a801d44383c126
Kamil Waligóra <k.waligora37@gmail.com>
2019-08-04 15:59:44 +0200

    Add MDI window

--- Symulacja/Source.cpp
+++ Symulacja/Source.cpp
@@ -4,20 +4,21 @@
 
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK ChildWindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
-void CreateFront(HWND hwnd, HINSTANCE hInstance);
+void CreateFront(HINSTANCE hInstance);
 
 char szClassName[] = "KlasaOkna";
 char szChildName[] = "KlasaOknaDziecka";
 
+HWND hwnd;
 HWND hMDIClient;
 HWND hMasaKulki;
 HWND hMasaPreta;
 HWND hPredkoscKulki;
 HWND hPrzycisk;
+HWND hChild;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 
-	HWND hwnd;
 	MSG messages;
 	WNDCLASSEX wincl;
 
@@ -64,6 +65,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	);
 
 	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
 
 	CLIENTCREATESTRUCT ccs;
 
@@ -74,9 +76,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		WS_CHILD | WS_CLIPCHILDREN | WS_VSCROLL | WS_HSCROLL,
 		5, 5, 700, 700, hwnd, (HMENU)0xCAC, hInstance, (LPSTR)& ccs);
 
-	ShowWindow(hMDIClient, SW_SHOW);
+	ShowWindow(hMDIClient, nCmdShow);
 
-	CreateFront(hwnd, hInstance);
+	CreateFront(hInstance);	
+
 
 	while (GetMessage(&messages, NULL, 0, 0)) {
 		if (!TranslateMDISysAccel(hMDIClient, &messages)) {
@@ -84,14 +87,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			DispatchMessage(&messages);
 		}
 	}
-
 	return messages.wParam;
-
 }
 
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	switch (message) {
-
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
@@ -107,15 +107,13 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
 LRESULT CALLBACK ChildWindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	switch (message) {
 
-		// tu mo¿esz daæ ró¿ne case'y
-
 	default:
 		return DefMDIChildProc(hwnd, message, wParam, lParam);
 	}
 	return 0;
 }
 
-void CreateFront(HWND hwnd, HINSTANCE hInstance) {
+void CreateFront(HINSTANCE hInstance) {
 	CreateWindowEx(0, "STATIC", "Masa Prêta:", WS_CHILD | WS_VISIBLE | SS_LEFT,
 		720, 30, 150, 20, hwnd, NULL, hInstance, NULL);
 
@@ -143,9 +141,26 @@ void CreateFront(HWND hwnd, HINSTANCE hInstance) {
 	CreateWindowEx(0, "STATIC", "Km/h", WS_CHILD | WS_VISIBLE | SS_LEFT,
 		890, 130, 150, 20, hwnd, NULL, hInstance, NULL);
 
-
 	hPrzycisk = CreateWindowEx(0, "BUTTON", "Start", WS_CHILD | WS_VISIBLE, 745, 180, 150, 30, hwnd, NULL, hInstance, NULL);
 
 	HWND Footer = CreateWindowEx(0, "STATIC", "Kamil Waligóra 2019", WS_CHILD | WS_VISIBLE | SS_RIGHT,
 		730, 700, 150, 20, hwnd, NULL, hInstance, NULL);
+
+	MDICREATESTRUCT mcs;
+
+	mcs.szTitle = "Nowe okno MDI";
+	mcs.szClass = szChildName; // zak³adam, ¿e korzystasz z szablonu (punkt 1.5) 
+	mcs.hOwner = GetModuleHandle(NULL); // pobieramy instancjê aplikacji
+	mcs.x = 0;
+	mcs.y = 0;
+	mcs.cx = 700; // domyœlne wymiary
+	mcs.cy = 700;
+	mcs.style &= ~WS_MAXIMIZE;
+
+	hChild = (HWND)SendMessage(hMDIClient, WM_MDICREATE, 0, (LONG)& mcs);
+
+	if (!hChild) {
+		MessageBox(hwnd, "Nie uda³o siê utworzyæ okienka MDI!", "A to szkoda...",
+			MB_ICONEXCLAMATION | MB_OK);
+	}
 }
